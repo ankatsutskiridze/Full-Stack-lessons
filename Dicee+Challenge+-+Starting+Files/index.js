f9a0f3b90bcc4d31ce0b3bfddf46fa3746300b6e
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-23 16:15:40 +0400

    Update dice image source logic in index.js for improved clarity and functionality

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,5 +1,5 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var diceImage = "images/dice" + randomNumber + ".png";
-document.getElementById("dice1").setAttribute("src", diceImage);
-
+var randomNumber = Math.floor(Math.random() * 6) + 1; // 1-დან 6-მდე რანდომი
+var diceImage = "./images/dice" + randomNumber + ".png"; // შექმნილია სურათის სახელი
+console.log(diceImage);
+document.querySelector(".img1").setAttribute("src", diceImage); // მარცხენა <img> ჩასმა
 console.log(randomNumber);
