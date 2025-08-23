5e702a9d76c7ad83a73b71c214100dbd3381988b
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-23 18:51:42 +0400

    Remove commented debug logs and clean up code in index.js

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,16 +1,11 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1; // 1-დან 6-მდე რანდომი
-var diceImage = "./images/dice" + randomNumber + ".png"; // შექმნილია სურათის სახელი
-console.log(diceImage);
-document.querySelector(".img1").setAttribute("src", diceImage); // მარცხენა <img> ჩასმა
-console.log(randomNumber);
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var diceImage = "./images/dice" + randomNumber + ".png";
+document.querySelector(".img1").setAttribute("src", diceImage);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var diceImage2 = "./images/dice" + randomNumber2 + ".png";
-console.log(diceImage2);
 document.querySelector(".img2").setAttribute("src", diceImage2);
-console.log(randomNumber2);
 
-// 🏆 გამარჯვებულის გამოცხადება
 if (randomNumber > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 } else if (randomNumber2 > randomNumber) {
