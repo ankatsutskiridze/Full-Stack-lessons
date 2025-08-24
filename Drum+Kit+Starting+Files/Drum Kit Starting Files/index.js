202940a543e8702a3435a4bda5ba6a1003a63614
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-24 22:20:35 +0400

    Fix buttonInnerHTML reference in handleClick function for audio playback

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -3,9 +3,9 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
-
+let buttonInnerHTML = this.innerHTML;
 function handleClick() {
-  switch (this.innerHTML) {
+  switch (buttonInnerHTML) {
     case "w":
       var audio = new Audio("./sounds/tom-1.mp3");
       audio.play();
