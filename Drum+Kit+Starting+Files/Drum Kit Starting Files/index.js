bc87c596b41d85fc9cca59ca213bd5ca4be39051
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-24 22:17:31 +0400

    Refactor handleClick function to play corresponding audio for each drum button

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -5,8 +5,37 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
 }
 
 function handleClick() {
-  alert("I got clicked!");
-}
+  switch (numberOfDrumButtons) {
+    case 1:
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case 2:
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case 3:
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case 4:
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case 5:
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case 6:
+      var audio = new Audio("./sounds/kick.mp3");
+      audio.play();
+      break;
+    case 7:
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
 
-var audio = new Audio("audio_file.mp3");
-audio.play();
+    default:
+      break;
+  }
+}
