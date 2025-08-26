91eb3da238abe6567975df9bd10dc7d7dea03d04
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-27 00:53:26 +0400

    edd function nextSequence

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,12 @@
-alert("Hello! JavaScript is working.");
+buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 3);
+  var randomChosenColor = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
