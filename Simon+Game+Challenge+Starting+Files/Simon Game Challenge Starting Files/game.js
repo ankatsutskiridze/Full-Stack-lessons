a53e7c415082b3aa2133980176b342588bfc8124
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-27 02:02:19 +0400

    add function startOver

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -7,39 +7,94 @@ var gamePattern = [];
 // მომხმარებლის მიერ დაკლიკილი მიმდევრობა
 var userClickedPattern = [];
 
+// Level
+var level = 0;
+
+// თამაშის დაწყების სტატუსი
+var started = false;
+
+// კლავიშზე დაჭერის დამჭერი
+$(document).keydown(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+// ღილაკზე დაკლიკების დამჭერი
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  // მომხმარებლის პასუხების შემოწმება
+  checkAnswer(userClickedPattern.length - 1);
+});
+
 // ფუნქცია, რომელიც ქმნის ახალ შემთხვევით ფერს
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4); // 0-დან 3-მდე
+  userClickedPattern = []; // მომხმარებლის მიმდევრობა იფარება ყოველი ახალი Level-ზე
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColours[randomNumber];
   gamePattern.push(randomChosenColor);
 
-  // ღილაკის ანიმაცია (მიბრწყინება)
   $("#" + randomChosenColor)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
 
-  // შესაბამისი ხმის დაკვრა
   playSound(randomChosenColor);
 }
 
-// ღილაკზე დაკლიკების დამჭერი
-$(".btn").click(function () {
-  // ვინ დაჭირა - ღილაკის id
-  var userChosenColour = $(this).attr("id");
+// მომხმარებლის პასუხების შემოწმების ფუნქცია
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
 
-  // მომხმარებლის არჩევანის დამატება
-  userClickedPattern.push(userChosenColour);
+    // თუ მომხმარებელმა დაამთავრა სექვენსი
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
 
-  // კონსოლში გამოვიტანოთ, რომ ვნახოთ შედეგი
-  console.log(userClickedPattern);
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
 
-  // ხმის დაკვრა
-  playSound(userChosenColour);
-});
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+// თამაშის თავიდან დაწყების ფუნქცია
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
 
 // ხმის დაკვრის ფუნქცია
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+// ანიმაციის ფუნქცია
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
