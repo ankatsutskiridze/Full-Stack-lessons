c2bf8826fe2da5adcd08201ba8610fb3cee782c9
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-08-31 13:38:11 +0400

    import inquirer from "inquirer";

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -1,3 +1,20 @@
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([
+    /* აქ ჩაწერე შენი კითხვები */
+  ])
+  .then((answers) => {
+    // აქ გამოიყენე მომხმარებლის პასუხები... რისთვისაც გჭირდება!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // ამ გარემოში კითხვა ვერ გამოისახა (ტერმინალი არ უჭერს მხარს)
+    } else {
+      // სხვა ტიპის შეცდომა მოხდა
+    }
+  });
+
 /* 
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
