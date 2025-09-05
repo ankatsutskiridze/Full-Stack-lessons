7a919588e38449e74353d28e97ed8ec1b29e5c6b
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-09-05 23:38:39 +0400

    Todo

--- 5.4+API+Authentication/index.js
+++ 5.4+API+Authentication/index.js
@@ -3,57 +3,77 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
+
+// API-ის საბაზისო მისამართი
 const API_URL = "https://secrets-api.appbrewery.com/";
 
-//TODO 1: Fill in your values for the 3 types of auth.
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+// TODO 1: შეავსეთ თქვენი მონაცემები ავტორიზაციის 3 ტიპისთვის.
+const yourUsername = ""; // თქვენი მომხმარებლის სახელი
+const yourPassword = ""; // თქვენი პაროლი
+const yourAPIKey = ""; // თქვენი API გასაღები
+const yourBearerToken = ""; // თქვენი Bearer ტოკენი
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
-  //TODO 2: Use axios to hit up the /random endpoint
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+// ენდფოინთი, რომელიც არ საჭიროებს ავტორიზაციას
+app.get("/noAuth", async (req, res) => {
+  // TODO 2: გამოიყენეთ axios, რომ გამოიძახოთ /random ენდფოინთი
+  try {
+    const result = await axios.get(API_URL + "/random");
+    // მიღებული მონაცემები გადაეცით EJS ფაილს "content" ცვლადში
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/basicAuth", (req, res) => {
-  //TODO 3: Write your code here to hit up the /all endpoint
-  //Specify that you only want the secrets from page 2
-  //HINT: This is how you can use axios to do basic auth:
-  // https://stackoverflow.com/a/74632908
-  /*
-   axios.get(URL, {
+// ენდფოინთი Basic Auth-ისთვის
+app.get("/basicAuth", async (req, res) => {
+  // TODO 3: გამოიძახეთ /all ენდფოინთი Basic Auth-ის გამოყენებით
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
       auth: {
-        username: "abc",
-        password: "123",
+        username: yourUsername,
+        password: yourPassword,
       },
     });
-  */
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/apiKey", (req, res) => {
-  //TODO 4: Write your code here to hit up the /filter endpoint
-  //Filter for all secrets with an embarassment score of 5 or greater
-  //HINT: You need to provide a query parameter of apiKey in the request.
+// ენდფოინთი API Key-სთვის
+app.get("/apiKey", async (req, res) => {
+  // TODO 4: გამოიძახეთ /filter ენდფოინთი და გაფილტრეთ 5-ზე მაღალი ქულით
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
-app.get("/bearerToken", (req, res) => {
-  //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
-  //and get the secret with id of 42
-  //HINT: This is how you can use axios to do bearer token auth:
-  // https://stackoverflow.com/a/52645402
-  /*
-  axios.get(URL, {
-    headers: { 
-      Authorization: `Bearer <YOUR TOKEN HERE>` 
-    },
-  });
-  */
+// ენდფოინთი Bearer Token-ისთვის
+app.get("/bearerToken", async (req, res) => {
+  // TODO 5: გამოიძახეთ /secrets/{id} ენდფოინთი და მიიღეთ საიდუმლო id 42-ით
+  try {
+    const result = await axios.get(API_URL + "/secrets/42", {
+      headers: {
+        Authorization: `Bearer ${yourBearerToken}`,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(port, () => {
