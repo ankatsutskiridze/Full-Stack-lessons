3943f630f52c8ec6606d3bdc85c0b40fea7a7631
AnkaTsutskiridze <anka.tsutskiridze2000@gmail.com>
2025-09-08 02:11:24 +0400

    add import pg

--- 8.2+Postgres+READ/index.js
+++ 8.2+Postgres+READ/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "Riverdeili1",
+  port: 5432,
+});
 
 let totalCorrect = 0;
 
--- 8.2+Postgres+READ/solution.js
+++ 8.2+Postgres+READ/solution.js
@@ -5,8 +5,8 @@ import pg from "pg";
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "world",
-  password: "123456",
+  database: "postgres",
+  password: "Riverdeili1",
   port: 5432,
 });
 
